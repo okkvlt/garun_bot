4aa56164b8f0d06ebd062b8b9bfdfda39c9afa3c
Ruan <occult.aiwass@gmail.com>
2022-04-07 16:41:05 -0300

    feat: help message

--- bot.py
+++ bot.py
@@ -22,11 +22,44 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     if message.content == "$help":
-        message_help = message.channel.send(f"Olá {message.author}! Seja bem vindo ao Garun BOT!\n")
+        
+        help_message = f"""
+Olá {message.author}! Seja bem vindo ao Garun BOT!
+
+Segue a lista de comandos para o uso do bot:
+
+$help: mostra essa mensagem;
+
+DJ:
+
+$play [spotify music/playlist]: reproduz a música ou playlist passada por argumento;
+$queue: mostra a lista de reprodução;
+$pause: pausa a reprodução;
+$resume: despausa a reprodução;
+$stop: desconecta o bot da call;
+$next: avança para a próxima música;
+$back: retorna para a música passada;
+$goto [n]: avança ou retorna para a música da posição n da lista;
+$clear: limpa a lista de reprodução;
+$add [spotify music/playlist]: adiciona música ou playlist na lista de reprodução;
+$remove [n]: remove a música da posição n da lista;
+$volume [0-100]: altera o volume do bot;
+
+LAST.FM:
+
+$connect: conecta o BOT com sua conta do last.fm;
+$scrobble [on/off]: ativa/desativa o modo scrobble;
+$edit: edita os dados do scrobble em execução;
+$skip: interrompe o scrobble em execução;
+$disconnect: desconecta o BOT da sua conta do last.fm;
+"""
+        
+        message_help = message.channel.send(help_message)
         
         await message_help
     
     if message.author == bot.user and "Seja bem vindo" in message.content:
         await message.add_reaction('❤️')
+        await message.add_reaction('🕊️')
 
 bot.run(TOKEN)
