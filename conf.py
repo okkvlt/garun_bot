861a3c3fdd9c70fd6c3047cd45a15b17a611014a
Ruan <occult.aiwass@gmail.com>
2022-04-08 10:06:42 -0300

    fix: Config corrigido.

--- conf.py
+++ conf.py
@@ -7,7 +7,3 @@ TOKEN = config('BOT_TOKEN')
 # last.fm api
 API_KEY = config('API_KEY')
 API_SECRET = config('API_SECRET')
-
-# last.fm acc
-LAST_USER = config("LAST_USER")
-LAST_PASS = pylast.md5(config("LAST_PASS"))
