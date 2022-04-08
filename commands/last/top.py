c34b43fa4a82b899ef7abf536801f7b4622e18db
Ruan <occult.aiwass@gmail.com>
2022-04-08 16:14:10 -0300

    refactor: Melhora no código!

--- commands/last/top.py
+++ commands/last/top.py
@@ -1,64 +1,12 @@
 import discord
 from bot import bot
-from commands.last.utils import get_topArtists
+from commands.last.utils import top
 
 
 @bot.event
 async def on_message(message):
     if "$top_artists" in message.content:
-        data = message.content.split()
+        return await top(message, 2)
 
-        embed_last = discord.Embed(colour=0xedd58d)
-
-        embed_last.set_author(name="Garun — Top artistas",
-                              icon_url='https://i.imgur.com/59qD9SY.jpg')
-
-        embed_last.set_footer(
-            text=f"Powered by {bot.user}", icon_url='https://i.imgur.com/59qD9SY.jpg')
-
-        if len(data) != 4:
-            embed_last.add_field(name="Status", value="""
-*Formato inválido!*
-""", inline=False)
-
-            embed_last.add_field(name="Utilize:", value="""
-`$top_artists (user) (n) (overall/7days/1month/12month)`
-""", inline=False)
-
-            embed_last.add_field(name="Parâmetros:", value="""
-**user:** *usuário.*
-**n:** *top (n).*
-**(overall/7day/1month/12month):** *período de tempo.*
-""", inline=False)
-
-            return await message.channel.send(embed=embed_last)
-
-        user = data[1]
-        n = data[2]
-        time = data[3]
-
-        r = get_topArtists(user, n, time)
-
-        if not "error" in r:
-            top = r["topartists"]["artist"]
-
-            embed_last.set_author(name="Garun — Top "+n+" artistas",
-                                  icon_url='https://i.imgur.com/59qD9SY.jpg')
-
-            value = ""
-            for artist in top:
-                value += "**"+artist["@attr"]["rank"]+"º** — *" + \
-                    artist["name"]+" ("+artist["playcount"]+" plays).*\n"
-
-            embed_last.add_field(
-                name="Usuário: ", value="`"+user+"`", inline=False)
-
-            embed_last.add_field(name="Top "+str(n) +
-                                 " artistas", value=value, inline=False)
-
-            return await message.channel.send(embed=embed_last)
-
-        embed_last.add_field(name="Status", value="Erro: " +
-                             str(r["error"])+"\nMensagem: `"+r["message"]+"`", inline=False)
-
-        return await message.channel.send(embed=embed_last)
+    if "$top_albums" in message.content: #1
+        return await top(message, 1)
--- commands/last/utils.py
+++ commands/last/utils.py
@@ -1,5 +1,8 @@
+import discord
+
 import requests
 from conf import API_KEY
+from bot import bot
 
 
 def get_token():
@@ -14,3 +17,94 @@ def get_topArtists(user, n, time):
                      user+"&api_key="+API_KEY+"&limit="+n+"&period="+time+"&format=json")
 
     return r.json()
+
+
+def get_topAlbums(user, n, time):
+    r = requests.get("https://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=" +
+                     user+"&api_key="+API_KEY+"&limit="+n+"&period="+time+"&format=json")
+
+    return r.json()
+
+
+def top(message, id):
+    data = message.content.split()
+
+    embed_last = discord.Embed(colour=0xedd58d)
+
+    embed_last.set_author(name="Garun — Top albums",
+                          icon_url='https://i.imgur.com/59qD9SY.jpg')
+
+    embed_last.set_footer(
+        text=f"Powered by {bot.user}", icon_url='https://i.imgur.com/59qD9SY.jpg')
+
+    if len(data) != 4:
+        embed_last.add_field(name="Status", value="""
+*Formato inválido!*
+""", inline=False)
+
+        if id == 1:
+            embed_last.add_field(name="Utilize:", value="""
+`$top_albums (user) (n) (overall/7days/1month/12month)`
+    """, inline=False)
+        else:
+            embed_last.add_field(name="Utilize:", value="""
+`$top_artists (user) (n) (overall/7days/1month/12month)`
+""", inline=False)
+
+        embed_last.add_field(name="Parâmetros:", value="""
+**user:** *usuário.*
+**n:** *top (n).*
+**(overall/7day/1month/12month):** *período de tempo.*
+""", inline=False)
+
+        return message.channel.send(embed=embed_last)
+
+    user = data[1]
+    n = data[2]
+    time = data[3]
+
+    if id == 1:
+        r = get_topAlbums(user, n, time)
+    else:
+        r = get_topArtists(user, n, time)
+
+    if not "error" in r:
+        if id == 1:
+            top = r["topalbums"]["album"]
+
+            embed_last.set_author(name="Garun — Top "+n+" albums",
+                                  icon_url='https://i.imgur.com/59qD9SY.jpg')
+
+        else:
+            top = r["topartists"]["artist"]
+
+            embed_last.set_author(name="Garun — Top "+n+" artistas",
+                                  icon_url='https://i.imgur.com/59qD9SY.jpg')
+
+        value = ""
+
+        if id == 1:
+            for album in top:
+                value += "**"+album["@attr"]["rank"]+"º**: *"+album["artist"]["name"]+" — " + \
+                    album["name"]+" ("+album["playcount"]+" plays).*\n"
+        else:
+            for artist in top:
+                value += "**"+artist["@attr"]["rank"]+"º** — *" + \
+                    artist["name"]+" ("+artist["playcount"]+" plays).*\n"
+
+        embed_last.add_field(
+            name="Usuário: ", value="`"+user+"`", inline=False)
+
+        if id == 1:
+            embed_last.add_field(name="Top "+str(n) +
+                                 " albums", value=value, inline=False)
+        else:
+            embed_last.add_field(name="Top "+str(n) +
+                                 " artistas", value=value, inline=False)
+
+        return message.channel.send(embed=embed_last)
+
+    embed_last.add_field(name="Status", value="Erro: " +
+                         str(r["error"])+"\nMensagem: `"+r["message"]+"`", inline=False)
+
+    return message.channel.send(embed=embed_last)
