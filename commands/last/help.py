42a411d9a86685456fe893cea5f1a141e4b9c8f8
Ruan <occult.aiwass@gmail.com>
2022-04-16 16:56:46 -0300

    refactor: Format help message

--- commands/last/help.py
+++ commands/last/help.py
@@ -10,16 +10,16 @@ async def help_(message):
                           icon_url='https://i.imgur.com/59qD9SY.jpg')
 
     embed_help.add_field(name="Comandos de LAST.FM", value="""
-`$connect` — *conecta do last.fm com o bot.*
-`$top_artists (user) (n) (overall/7day/1month/12month)` — *mostra o top (n) artistas do usuário no(s) último(s) (overall/7day/1month/12month).*
-`$top_albums (user) (n) (overall/7day/1month/12month)` — *mostra o top (n) albums do usuário no(s) último(s) (overall/7day/1month/12month).*
-`$scrobble (on/off)` — *ativa ou desativa os scrobbles.*
-`$edit` — *edita os dados do scrobble em execução.* [não implementado]
-`$stop` — *interrompe o scrobble em execução.* [não implementado]
-`$love` — *dá "amei" na música em execução.* [não implementado]
-`$unlove` — *retira o "amei" da música em execução.* [não implementado]
-`$collage (artists/albums) (7/30)` — *gera uma colagem com os artistas ou albums mais ouvidos nos últimos sete ou trinta dias.* [não implementado]
-`$disconnect` — *desconecta a conta.*
+`$connect` — *Conecta do last.fm com o bot.*
+`$top_artists (user) (n) (overall/7day/1month/12month)` — *Mostra o top (n) artistas do usuário no(s) último(s) (overall/7day/1month/12month).*
+`$top_albums (user) (n) (overall/7day/1month/12month)` — *Mostra o top (n) albums do usuário no(s) último(s) (overall/7day/1month/12month).*
+`$scrobble (on/off)` — *Ativa ou desativa os scrobbles.*
+`$edit` — *Edita os dados do scrobble em execução.* [não implementado]
+`$stop` — *Interrompe o scrobble em execução.* [não implementado]
+`$love` — *Dá "amei" na música em execução.* [não implementado]
+`$unlove` — *Retira o "amei" da música em execução.* [não implementado]
+`$collage (artists/albums) (7/30)` — *Gera uma colagem com os artistas ou albums mais ouvidos nos últimos sete ou trinta dias.* [não implementado]
+`$disconnect` — *Desconecta a conta.*
 """, inline=False)
 
     embed_help.set_footer(
