3667e5d84f810c578c7578c04cc0cce2a0576fd3
Ruan <occult.aiwass@gmail.com>
2022-04-18 14:10:17 -0300

    refactor: Init message change

--- ready/ready.py
+++ ready/ready.py
@@ -3,19 +3,29 @@ from bot import bot
 from conf import DB
 from commands.last.utils import get_users
 
+import os
+
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} is online!')
 
     if len(bot.guilds) != 0:
-        print(f'\n{bot.user} is in '+str(len(bot.guilds))+' guild(s):')
+        print(f'\n{bot.user} está em '+str(len(bot.guilds))+' guildas:\n')
 
+        print("[guild.id - guild.name]")
         for guild in bot.guilds:
             print(f"{guild.id} - {guild.name}")
 
-    if len(get_users()) != 0:
-        print("\n"+str(len(get_users()))+" sessões iniciadas:")
+    else:
+        print("Nenhuma guilda!")
 
+    if len(get_users()) != 0:
+        print("\n"+str(len(get_users()))+" sessões iniciadas:\n")
+
+        print("[user.id, last.fm acc, session_key, scrobbling]")
         for user in get_users():
             print(user)
+
+    else:
+        print("Nenhuma sessão!")
