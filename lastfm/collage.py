84b3c09722e8b04cc91f8382d5acb0d617aee23a
Ruan <occult.aiwass@gmail.com>
2022-04-21 11:16:22 -0300

    refactor: transformando if's em objetos literais

--- lastfm/collage.py
+++ lastfm/collage.py
@@ -9,9 +9,38 @@ async def collage(message):
     msg = message.content
     data = msg.split()
 
-    modes = ["albums", "artists"]
-    sizes = ["3x3", "4x4", "5x5", "10x10"]
     periods = ["overall", "7day", "1month", "12month"]
+
+    modes = {
+        "albums": COLLAGE.gen_top_albums_collage,
+        "artists": COLLAGE.gen_top_artists_collage
+    }
+
+    sizes = {
+        "3x3": {
+            "limit": 9,
+            "img_size": 999,
+            "art_size": 333
+        },
+
+        "4x4": {
+            "limit": 16,
+            "img_size": 1000,
+            "art_size": 250
+        },
+
+        "5x5": {
+            "limit": 25,
+            "img_size": 1000,
+            "art_size": 200
+        },
+
+        "10x10": {
+            "limit": 100,
+            "img_size": 2000,
+            "art_size": 200
+        }
+    }
 
     fail = getEmbed()
 
@@ -22,7 +51,7 @@ async def collage(message):
     fail.add_field(name="Syntax:",
                    value="*`$collage [modo] [user] [NxN] [período]`*",
                    inline=False)
-    
+
     fail.add_field(name="Params:",
                    value="""
                     • **Modo:** *'albums', 'artists'*;
@@ -40,10 +69,10 @@ async def collage(message):
     size = data[3]
     period = data[4]
 
-    if not mode in modes:
+    if mode not in modes.keys():
         return await message.channel.send(embed=fail)
 
-    if not size in sizes:
+    if not size in sizes.keys():
         return await message.channel.send(embed=fail)
 
     if not period in periods:
@@ -51,44 +80,16 @@ async def collage(message):
 
     await message.channel.send("Gerando colagem...! (Isso pode demorar alguns minutos)")
 
-    if size == "3x3":
-        limit = 9
-        img_size = 999
-        art_size = 333
-
-    if size == "4x4":
-        limit = 16
-        img_size = 1000
-        art_size = 250
-
-    if size == "5x5":
-        limit = 25
-        img_size = 1000
-        art_size = 200
-
-    if size == "10x10":
-        limit = 100
-        img_size = 2000
-        art_size = 200
-
     try:
-        if mode == "albums":
-            img = COLLAGE.gen_top_albums_collage(user,
-                                                 period,
-                                                 limit,
-                                                 art_width=art_size,
-                                                 art_height=art_size,
-                                                 collage_width=img_size,
-                                                 collage_height=img_size)
-
-        if mode == "artists":
-            img = COLLAGE.gen_top_artists_collage(user,
-                                                  period,
-                                                  limit,
-                                                  art_width=art_size,
-                                                  art_height=art_size,
-                                                  collage_width=img_size,
-                                                  collage_height=img_size)
+        mode_function = modes[mode]
+        collage_size = sizes[size]
+        img = mode_function(user,
+                            period,
+                            collage_size["limit"],
+                            art_width=collage_size["art_size"],
+                            art_height=collage_size["art_size"],
+                            collage_width=collage_size["img_size"],
+                            collage_height=collage_size["img_size"])
 
     except Exception as error:
         return await message.channel.send("**Erro ao gerar colagem!**\n**Erro:** *"+str(error)+"*")
